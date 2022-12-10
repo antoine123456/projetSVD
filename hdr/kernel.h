b228d9176f5fffcd7dee0a6ea489eae81fc35c41
jacky <martin.pecheur@riviere.kwaii>
2022-12-10 18:01:21 +0100

    fin calcul noyau

--- hdr/kernel.h
+++ hdr/kernel.h
@@ -6,20 +6,42 @@ typedef struct
     bool ok;
 } pivot_t;
 
+typedef struct
+{
+    double *A;
+    double *B;
+} imker_t;
+
+typedef struct
+{
+    int *col;
+    int size;
+} col_t;
+
+typedef struct
+{
+    double *vectors;
+    int size;
+} noyau_t;
+
+void __repr__(double *A, int n, int m);
+void defMat(double *A);
+int euclidean_division(double a, double b);
+double modulo(double x, double y);
 void echanger2(double *A, int j1, int j2, double *B, int nA, int nB);
 void mult2(double *A, int j, double t, double *B, int nA, int nB);
 void div2(double *A, int j, double t, double *B, int nA, int nB);
 void add2(double *A, int j1, double t, int j2, double *B, int nA, int nB);
 double gcd(double a, double b);
-void div(double *A, int j, double t, int n);
+void divide(double *A, int j, double t, int n);
 double gcdcol(double *A, int j, int n);
 void normaliser_colonne(double *A, int j, int n);
 void pivoter2(double *A, int l, int k, double *B, int nA, int nB);
 pivot_t chercher_pivot(double *A, int k, int n);
 double *eye(int n);
-double *imker(double *A, int n);
-double *transpose(double *A, int n);
+imker_t imker(double *A, int n);
+double *transpose(double *A, int n, int m);
 double *col(double *A, int j, int n);
 bool est_col_nulle(double *A, int j, int n);
-int *col_nulles(double *A, int n);
-double *noyau(double *A, int n);
+col_t col_nulles(double *A, int n);
+noyau_t noyau(double *A, int n);
--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,71 @@
-#include <kernel.h>
+#include "kernel.h"
+#include <stdio.h>
+#include <stdlib.h>
+void __repr__(double *A, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (A[i * n + j] < 0)
+            {
+                printf(" %f", A[i * n + j]);
+            }
+            else
+            {
+                printf("  %f", A[i * n + j]);
+            }
+        }
+        printf("\n");
+    }
+}
+void defMat(double *A)
+{
+    A[0] = 0;
+    A[1] = -9.0;
+    A[2] = -5.0;
+    A[3] = -5.0;
+    A[4] = 0;
+    A[5] = 9.0;
+    A[6] = 5.0;
+    A[7] = 5.0;
+    A[8] = 0;
+    A[9] = 0;
+    A[10] = 0;
+    A[11] = 0;
+    A[12] = 0;
+    A[13] = 0;
+    A[14] = 4.0;
+    A[15] = 4.0;
+}
 int euclidean_division(double a, double b)
 {
-    int q;
-    double r = a;
+    if ((b == 0) | (a == 0))
+        return 0;
+    int sign = ((a < 0) ^ (b < 0)) ? -1 : 1;
+    int q = 0;
+    b = (b < 0) ? -b : b;
+    double r = (a < 0) ? -a : a;
+    if (b > r && sign < 0)
+    {
+        double tmp = r;
+        r = b;
+        b = tmp;
+    }
     while (r >= b)
     {
         r = r - b;
         q += 1;
     }
-    return q;
+    return sign * q;
 }
 double modulo(double x, double y)
 {
     /*x modulo y*/
-    x -= y * abs(euclidean_division(x, y));
+    if (x == 0)
+        return 0;
+    int sign = ((x < 0) ^ (y < 0)) ? -1 : 1;
+    x = x - sign * y * abs(euclidean_division(x, y));
     return x;
 }
 void echanger2(double *A, int j1, int j2, double *B, int nA, int nB)
@@ -74,8 +126,9 @@ double gcd(double a, double b)
         a = b;
         b = modulo(tmp, b);
     }
+    return a;
 }
-void div(double *A, int j, double t, int n)
+void divide(double *A, int j, double t, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -96,13 +149,13 @@ void normaliser_colonne(double *A, int j, int n)
     double d = gcdcol(A, j, n);
     if (d != 0)
     {
-        div(A, j, d, n);
+        divide(A, j, d, n);
     }
 }
 void pivoter2(double *A, int l, int k, double *B, int nA, int nB)
 {
     double t = A[l * nA + k];
-    for (int j = k + 1; nA; j++)
+    for (int j = k + 1; j < nA; j++)
     {
         double u = A[l * nA + j];
         double d = gcd(t, u);
@@ -139,22 +192,22 @@ pivot_t chercher_pivot(double *A, int k, int n)
 }
 double *eye(int n)
 {
-    double *tmp = (double *)calloc(n, sizeof(double));
+    double *tmp = (double *)calloc(n * n, sizeof(double));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             if (j == i)
             {
-                tmp[i * n + j] = 0;
+                tmp[i * n + j] = 1;
             }
         }
     }
     return tmp;
 }
-double *imker(double *A, int n)
+imker_t imker(double *A, int n)
 {
-    double *copieA = (double *)malloc(n * sizeof(double));
+    double *copieA = (double *)malloc(n * n * sizeof(double));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -163,18 +216,117 @@ double *imker(double *A, int n)
         }
     }
     double *B = eye(n);
+    int nB = n;
     int k = 0;
     while (true)
     {
-        pivot_t res = chercher_pivot(A, k, n);
+        pivot_t res = chercher_pivot(copieA, k, n);
         if (!res.ok)
-        {
             break;
+        if (res.j != k)
+            echanger2(copieA, res.j, k, B, n, nB);
+        pivoter2(copieA, res.i, k, B, n, nB);
+        k++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        normaliser_colonne(copieA, i, n);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        normaliser_colonne(B, i, nB);
+    }
+    imker_t ret;
+    ret.A = copieA;
+    ret.B = B;
+    return ret;
+}
+double *transpose(double *A, int n, int m)
+{
+    if (n == 0)
+        return NULL;
+    double *B = (double *)calloc(n * n, sizeof(double));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            B[i * n + j] = A[i + j * n];
         }
     }
+    return B;
 }
-double *transpose(double *A, int n);
-double *col(double *A, int j, int n);
-bool est_col_nulle(double *A, int j, int n);
-int *col_nulles(double *A, int n);
-double *noyau(double *A, int n);
+double *col(double *A, int j, int n)
+{
+    double *res = (double *)calloc(n * n, sizeof(double));
+    for (int i = 0; i < n; i++)
+    {
+        res[i] = A[i * n + j];
+    }
+    return res;
+}
+bool est_col_nulle(double *A, int j, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i * n + j] != 0)
+            return false;
+    }
+    return true;
+}
+col_t col_nulles(double *A, int n)
+{
+    int *cols = (int *)calloc(n, sizeof(int));
+    int size = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (est_col_nulle(A, i, n))
+        {
+            cols[i] = i + 1;
+            size++;
+        }
+    }
+    int *col = (int *)calloc(size, sizeof(int));
+    int iter = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (cols[i] != 0)
+        {
+            col[iter] = cols[i] - 1;
+            iter++;
+        }
+    }
+    free(cols);
+    col_t ret;
+    ret.col = col;
+    ret.size = size;
+    return ret;
+}
+noyau_t noyau(double *A, int n)
+{
+    imker_t res = imker(A, n);
+    col_t nulles = col_nulles(res.A, n);
+    double *tmp;
+    double *ret = (double *)malloc(n * n * sizeof(double));
+    for (int i = 0; i < nulles.size; i++)
+    {
+        tmp = col(res.B, nulles.col[i], n);
+        for (int j = 0; j < n; j++)
+        {
+            ret[i * n + j] = tmp[j];
+        }
+    }
+    double *transp = transpose(ret, n, nulles.size);
+    noyau_t ker;
+    ker.vectors = transp;
+    ker.size = nulles.size;
+    free(ret);
+    return ker;
+}
+int main(int argc, char **argv)
+{
+    double *A = (double *)calloc(4 * 4, sizeof(double));
+    defMat(A);
+    noyau_t res = noyau(A, 4);
+    __repr__(res.vectors, 4, res.size);
+    return 0;
+}
